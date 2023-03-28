7383536e058aade67be5922582c42f5ec5b1f09e
JoaoSGabriel <joaogabriel0359@gmail.com>
2023-03-28 18:47:35 -0300

    create controller and DTO

---
+++ src/main/java/com/tastecamp/api/controller/CarController.java
@@ -0,0 +1,16 @@
+package com.tastecamp.api.controller;
+
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/car")
+public class CarController {
+    
+    @PostMapping
+    public void create(@RequestBody carDTO req) {
+        System.out.println(req);
+    }
+}
